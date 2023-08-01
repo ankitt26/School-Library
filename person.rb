0ef25bb5ac807f5cb20ab4595bb430798ff6aa14
Ankit <ankit26kushwa@gmail.comm>
2023-08-01 13:25:42 +0530

    move attr_reader and accessor before the initialize

--- person.rb
+++ person.rb
@@ -1,13 +1,13 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def of_age?
     @age >= 18

3b5f7ccb7e35d855e61e4e18102bc006f7852341
Ankit <ankit26kushwa@gmail.comm>
2023-08-01 10:57:04 +0530

    create class person

---
+++ person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name
+  attr_accessor :age
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? and @parent_permission
+  end
+
+  private :of_age?
+end

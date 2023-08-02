441500eed09cc7cba2e84b2a88c509f981b4dbd3
Ankit <ankit26kushwa@gmail.comm>
2023-08-02 11:02:15 +0530

    create class BaseDecorator < nameable

--- person.rb
+++ person.rb
@@ -4,6 +4,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age

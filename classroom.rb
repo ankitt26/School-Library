0550e0affd4304ea0bb89f8e93143cf8f3aba2f8
Ankit <ankit26kushwa@gmail.comm>
2023-08-03 21:25:48 +0530

    create the belongs-to side  in student.rb

--- classroom.rb
+++ classroom.rb
@@ -2,7 +2,7 @@ class Classroom
   attr_accessor :label
   attr_reader :students
 
-  def initialize
+  def initialize(label)
     @label = label
     @students = []
   end
--- student.rb
+++ student.rb
@@ -2,12 +2,15 @@ require_relative 'person'
 require_relative 'classroom'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
-    @classroom.add_student(self) if classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky

3cdfb4738790b99dc553da55210bdfe395726ff8
Ankit <ankit26kushwa@gmail.comm>
2023-08-03 16:23:03 +0530

    has-many relationship b/w classroom and student

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @classroom.add_student(self) if classroom
   end
 
   def play_hooky

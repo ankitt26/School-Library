ada307369d92467aeee930752df754b3ab321603
Ankit <ankit26kushwa@gmail.comm>
2023-08-04 18:47:54 +0530

    Add create_person method

--- app.rb
+++ app.rb
@@ -23,4 +23,33 @@ class App
 
     puts "#{title} by #{author} created"
   end
+
+  def create_person
+    print "Do you want to create a student (1) or teacher (2)? [Input the number]: "
+    person_type = gets.chomp
+
+    if person_type == "1"
+      print "Enter student name: "
+      name = gets.chomp
+
+      print "Enter student age: "
+      age = gets.chomp
+
+      print "Does the student have parent permission? [Y/N]: "
+      parent_permission = gets.chomp
+
+      type = "student"
+    else
+      print "Enter teacher name: "
+      name = gets.chomp
+
+      print "Enter teacher Age"
+      age = gets.chomp
+
+      print "Enter teacher specialization: "
+      specialization = gets.chomp
+
+      type = "teacher"
+    end
+  end
 end

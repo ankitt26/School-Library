e6c175ba40be95c164eccb0fb4d64ac6730c03fe
Ankit <ankit26kushwa@gmail.comm>
2023-08-04 18:40:55 +0530

    create list_book method

--- app.rb
+++ app.rb
@@ -3,5 +3,12 @@ require_relative 'book'
 class App
   def initialize()
     @books = []
+    @people = []
+  end
+
+  def list_book
+    @books.each do |book|
+      puts "#{book.title} by #{book.author}"
+    end
   end
 end

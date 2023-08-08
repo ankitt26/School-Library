77992649fbd61b919714dc44899f362a83f50f22
Ankit <ankit26kushwa@gmail.comm>
2023-08-08 12:02:21 +0530

    Add display_menu and user_input methods in main.rb

--- main.rb
+++ main.rb
@@ -9,18 +9,10 @@ class Main
     @app = App.new
   end
 
-  def main
+  def start
     loop do
-      puts
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      choice = gets.chomp.to_i
+      display_menu
+      choice = user_input
 
       if (1..6).include?(choice)
         user_choice(choice)
@@ -31,6 +23,22 @@ class Main
         puts 'Invalid choice. Please try again.'
       end
     end
+  end
+
+  def display_menu
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def user_input
+    gets.chomp.to_i
   end
 
   def user_choice(choice)
@@ -53,4 +61,4 @@ end
 
 # Run the main program
 main_app = Main.new
-main_app.main
+main_app.start

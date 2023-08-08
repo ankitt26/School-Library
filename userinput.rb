00f3b47550ea7cc45ca72527697646437122d295
Ankit <ankit26kushwa@gmail.comm>
2023-08-08 15:56:17 +0530

    Add module userinput

---
+++ userinput.rb
@@ -0,0 +1,24 @@
+require_relative 'app'
+
+module UserInput
+  def initialize
+    @app = App.new
+  end
+
+  def user_choice(choice)
+    case choice
+    when 1
+      @app.list_book
+    when 2
+      @app.list_people
+    when 3
+      @app.create_person
+    when 4
+      @app.create_book
+    when 5
+      @app.create_rental
+    when 6
+      @app.list_rental
+    end
+  end
+end

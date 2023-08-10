7673ddbdba5ed2c00137ca49eb467658edfca3e8
Ankit <ankit26kushwa@gmail.comm>
2023-08-10 20:45:48 +0530

    Add tests for classroom ..create classroom_spec.rb

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../library/classroom'
+
+describe Classroom do
+  before(:each) do
+    @class = Classroom.new('Math-234')
+  end
+
+  describe '#initialize' do
+    it 'creates a new Label class' do
+      expect(@class.label).to eq('Math-234')
+      expect(@class.students).to eq([])
+    end
+  end
+
+
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      student = double('student1')
+      expect(student).to receive(:classroom=).with(@class)
+      @class.add_student(student)
+
+      expect(@class.students).to include(student)
+    end
+  end
+end
